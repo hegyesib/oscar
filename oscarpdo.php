537d2c0910faf5da7af78a3533d8ba4ca116b871
hegyesi balazs <hegyesibalazs@gmail.com>
2025-03-10 12:07:31 +0100

    oscapdos miden+++++l

--- oscarpdo.php
+++ oscarpdo.php
@@ -2,25 +2,33 @@
 require("dbvezerlopdo.php");
 
 class Oscar {
+    
     private $db;
-    public function __construct(){
+
+    public function __construct() {
         $this->db = new DBController();
-    public function getAllOscars(){
+    }
+
+    public function getAllOscars() {
         $query = "SELECT m_ID, title, m_desc, pic FROM movie";
-        return $this->this->db->executeSelectQuery($query);
+        return $this->db->executeSelectQuery($query);
     }
-    public function getOscarsById($OscarId){
-        $query = "SELECT m_ID, title, m_desc, pic
-        FROM movie WHERE , m_ID = ?";
-        return $this->db->executeSelectQuery($query, []);
+
+    public function getOscarsById($OscarId) {
+        $query = "SELECT m_ID, title, m_desc, pic FROM movie WHERE m_ID = ?";
+        return $this->db->executeSelectQuery($query, [$OscarId]);
     }
-    public function getOscarsByType($Mt_name){
-        $query = "SELECT m_ID, title, m_desc, pic, movie_type.Mt_description
-        FROM movie
-        inner join movie_type on movie_type.mt_ID=movie.m_type
-        where movie_type.Mt_name= '".$Mt_name."'";
-        return $this->db->executeSelectQuery($query,)
+
+    public function getOscarsByType($Mt_name) {
+        $query = "SELECT m_ID, title, m_desc, pic, movie_type.Mt_description 
+                  FROM movie 
+                  INNER JOIN movie_type ON movie_type.mt_ID = movie.m_type 
+                  WHERE movie_type.Mt_name = ?";
+        return $this->db->executeSelectQuery($query, [$Mt_name]);
+    }
+
+    public function __destruct() {
+        $this->db->closeDB();
     }
 }
-
 ?>
